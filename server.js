5fdde45dce4cf7355d89cd71c20fef38cabf50a4
Nzei95 <nzeimark@gmail.com>
2019-05-04 20:57:13 +0100

    Added register user endpoint and password encryption

--- server.js
+++ server.js
@@ -1,27 +1,33 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 // Import routes
-const users = require('./routes/api/users');
-const posts = require('./routes/api/posts');
-const profile = require('./routes/api/profile');
+const users = require("./routes/api/users");
+const posts = require("./routes/api/posts");
+const profile = require("./routes/api/profile");
 
 const app = express();
 
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 // DB Config
-const db = require('./config/keys').mongoURI
+const db = require("./config/keys").mongoURI;
 
 // Connect to MongoDB
-mongoose.connect(db, { useNewUrlParser: true })
-        .then(() => console.log('MongoDB Connected'))
-        .catch(err => console.log(err))
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send('Hello World'));
+app.get("/", (req, res) => res.send("Hello World"));
 
 // Use routes
-app.use('/api/users', users);
-app.use('/api/posts', posts);
-app.use('/api/profile', profile);
+app.use("/api/users", users);
+app.use("/api/posts", posts);
+app.use("/api/profile", profile);
 
 const port = process.env.PORT || 5000;
 
